b70edd4af4af569de30c3e74b501a622638c763c
Owen Voke <development@voke.dev>
2024-05-29 13:37:02 +0100

    style: inline unnecessary variable

--- src/utils/internal/buildRequestUrl.ts
+++ src/utils/internal/buildRequestUrl.ts
@@ -4,9 +4,7 @@ export const buildRequestUrl = (
     args: Record<string, string | number> = {},
 ) => {
     const concatenated = `${baseUrl}/${endpointUrl}`;
-    const withoutDoubleSlashes = concatenated.replaceAll(/([^:]\/)\/+/g, "$1");
-
-    let withArgs = withoutDoubleSlashes;
+    let withArgs = concatenated.replaceAll(/([^:]\/)\/+/g, "$1");
 
     const queryParamValues: Record<string, string> = {};
 
